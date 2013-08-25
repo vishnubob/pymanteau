8a4e0fb4548d1390226c33bb5efe307faf0d9d71
Giles Hall <giles@polymerase.org>
2013-08-25 16:03:01 -0400

    better translation system

--- pymanteau.py
+++ pymanteau.py
@@ -5,7 +5,7 @@ from dxfwrite import DXFEngine as dxf
 class Context(object):
     def __init__(self, fn):
         self.canvas = dxf.drawing(fn)
-        canvas.add_layer('LINES')
+        self.canvas.add_layer('LINES')
         self.config = [{}]
         self.translation_stack = []
         self.rotation_stack = []
@@ -38,7 +38,7 @@ class Context(object):
         self.translation_stack.pop()
 
     def convert(self, point):
-        return tuple([eval(str(pos), self.config) for pos in point])
+        return tuple([eval(str(pos), globals(), self.config[-1]) for pos in point])
         
     def translate(self, point):
         for offset in self.translation_stack:
@@ -64,21 +64,21 @@ class DrawShape(object):
 
     @property
     def config(self):
-        return self.content.config
+        return self.context.config
     
     def transform(self, coords):
         _coords = []
         for point in coords:
-            point = self.context.transform(point, self.config)
+            point = self.context.transform(point)
             _coords.append(point)
         return _coords
 
-    def draw(self, canvas, **args):
+    def draw(self, **args):
         for (opname, coords) in self.Operations:
             op = getattr(dxf, opname)
             coords = self.transform(coords)
             print coords
-            canvas.add(op(*coords, **args))
+            self.context.canvas.add(op(*coords, layer="LINES", **args))
 
 class TabShape(DrawShape):
     Operations = (
@@ -139,32 +139,13 @@ class TabStrip(DrawShape):
         self.context.pop()
 
 class BoxFace(QuadShape):
-    def _draw(self, canvas, **args):
-        # transform us to our center
-        super(BoxFace, self).draw(canvas, **args)
-        self.context.append(Translation(("face_width / 2.0", "face_height / 2.0")))
-        # bottom
-        self.context.append(Translation(("-tab_width / 2.0", "-face_height / 2.0")))
-        tm = TabShape(self.context, self.config)
-        #tm.draw(canvas, **args)
-        # left
-        self.context[0] = Rotation(90)
-        self.context[-1] = Translation(("face_width / 2.0", "-tab_width / 2.0"))
-        #tm.draw(canvas, **args)
-        # right
-        self.context[0] = Rotation(90)
-        #self.context[-1] = Translation(("-face_width / 2.0", "tab_width / 2.0"))
-        #tm.draw(canvas, **args)
-        self.context[-1] = Translation(("face_width / 2.0", "-face_height / 2.0"))
-        ts = TabStrip(self.context, self.config)
-        ts.draw(canvas, **args)
-        # top
-        self.context[0] = Rotation(0)
-        #self.context[-1] = Translation(("tab_width / 2.0", "face_height / 2.0"))
-        self.context[-1] = Translation(("-face_width / 2.0", "-face_height / 2.0"))
-        #ts = TabStrip(self.context, self.config, strip_vertical=False)
-        #ts.draw(canvas, **args)
-        #tm.draw(canvas, **args)
+    def draw(self, **args):
+        super(BoxFace, self).draw(**args)
+        self.context.push_config(tab_width=5, tab_height=2)
+        self.context.push_rotation(180)
+        self.context.push_translation((0, "(face_height / 2.0) - (tab_height / 2.0)"))
+        tm = TabShape(self.context)
+        tm.draw()
 
 class BoxFactory(object):
     Defaults = {
@@ -173,11 +154,13 @@ class BoxFactory(object):
     }
 
     def __init__(self, fn="box.dxf"):
-        context = self.context(fn)
+        context = Context(fn)
         context.push_translation((50, 50))
-        qs = QuadShape(context)
-        qs.draw()
-        self.context.save()
+        context.push_config(**self.Defaults)
+        bf = BoxFace(context)
+        bf.draw()
+        context.save()
         
 bf = BoxFactory()
+os.system("sync")
 os.system("inkscape -z box.dxf -e box.png")
