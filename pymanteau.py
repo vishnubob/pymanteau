9ad534b95d6f5fd8a4b9ed35df1e96f649cac054
Giles Hall <giles@polymerase.org>
2013-08-24 23:08:42 -0400

    translation matrix done

--- pymanteau.py
+++ pymanteau.py
@@ -1,44 +1,106 @@
 from dxfwrite import DXFEngine as dxf
+import math
 
-
-class CutMacro(object):
-    DEFAULTS = {}
-
-    def __init__(self, canvas, layer="LINES", offset=(0,0), rotation=0, color=0, **kw):
-        self.canvas = canvas
-        self.layer = layer
-        self.offset = offset
-        self.rotation = rotation
-        self.color = color
-        params = self.Defaults.copy()
-        params.update(kw)
-        self.__dict__.update(params)
-    
-    def draw(self):
+class Transform(object):
+    def transform(self, point):
         pass
 
-class ScrewReceiverMacro(CutMacro):
+class Translation(Transform):
+    def __init__(self, offset=(0, 0)):
+        self.offset = offset
+
+    def transform(self, point):
+        return map(sum, zip(self.offset, point))
+
+class Rotation(Transform):
+    def __init__(self, degrees=0, radians=None):
+        if radians != None:
+            self.radians = None
+        else:
+            self.radians = math.radians(degrees)
+
+    def transform(self, point):
+        x = point[0] * math.cos(self.radians) - point[1] * math.sin(self.radians)
+        y = point[0] * math.sin(self.radians) + point[1] * math.cos(self.radians)
+        return (x, y)
+
+class TransformStack(list):
+    def transform(self, point):
+        _point = point[:]
+        for step in self:
+            _point = step.transform(_point)
+        return _point
+    
+class CutMacro(object):
+    Defaults = {}
+
+    def __init__(self, canvas, stack=None, layer="LINES", color=0, **kw):
+        self.canvas = canvas
+        self.layer = layer
+        if stack == None:
+            stack = TransformStack()
+        self.stack = stack
+        self.color = color
+        self.params = self.Defaults.copy()
+        self.params.update(kw)
+    
+    def transform(self, coords):
+        _coords = []
+        for point in coords:
+            point = [self.params.get(pos, pos) for pos in point]
+            point = self.stack.transform(point)
+            _coords.append(point)
+        return _coords
+
     def draw(self):
-        drawing = dxf.drawing('test.dxf')
-        drawing.add_layer('LINES')
-        drawing.add(dxf.line((0, 0), (1, 0), color=7, layer='LINES'))
-        drawing.save()
+        for (opname, coords) in self.Operations:
+            op = getattr(dxf, opname)
+            coords = self.transform(coords)
+            print coords
+            self.canvas.add(op(*coords, layer=self.layer, color=self.color))
 
 class TabMacro(CutMacro):
-    DEFAULT = {
+    Defaults = {
         'width': 10,
         'height': 2,
     }
 
-    def draw(self):
-        drawing.add(dxf.line((0, 0), (1, 0), color=7, layer='LINES'))
+    Operations = (
+        ("line", ((0, 0), (0, "height"))),
+        ("line", ((0, "height"), ("width", "height"))),
+        ("line", (("width", "height"), ("width", 0))),
+    )
 
+class QuadMacro(CutMacro):
+    Defaults = {
+        'width': 40,
+        'height': 40,
+    }
+
+    Operations = (
+        ("line", ((0, 0), (0, "height"))),
+        ("line", ((0, "height"), ("width", "height"))),
+        ("line", (("width", "height"), ("width", 0))),
+        ("line", (("width", 0), (0, 0))),
+    )
+    
 class BoxFactory(object):
     def __init__(self, fn="box.dxf"):
         drawing = dxf.drawing(fn)
         drawing.add_layer('LINES')
-        drawing.add(dxf.line((0, 0), (1, 0), color=7, layer='LINES'))
+        stack = TransformStack([Translation((10, 10))])
+        t = Translation((10, 10))
+        stack.append(t)
+        quad = QuadMacro(drawing, stack=stack)
+        quad.draw()
+        tm = TabMacro(drawing, stack=stack)
+        tm.draw()
+        t = Translation((20, 0))
+        stack.append(t)
+        tm.draw()
+        t = Rotation(90)
+        stack.append(t)
+        tm.draw()
         drawing.save()
         
 bf = BoxFactory()
-
